a1a7b3ccf235a90e19b7f86abe225ad642412b93
Ahmed Ehab <ahmed.ehab5010@gmail.com>
2024-07-14 03:08:38 +0300

    Correct compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.ahmedehab.com',
     integrations: [
-        compress(),
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
-        })
+        }),
+        (await import('astro-compress')).default()
     ]
 });
